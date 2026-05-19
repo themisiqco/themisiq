643fb7a44ceb453603b57254f1221a764f31e922
Lisa Foster <lisacarolinefoster@me.com>
2026-05-19 10:32:45 -0400

    Increase logo size in nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -30,7 +30,7 @@ export default function Nav() {
       }}>
         {/* LOGO */}
         <a href="/" style={{ textDecoration: 'none', flexShrink: 0 }}>
-          <img src="/logo.png" alt="ThemisIQ" style={{ height: 32, width: 'auto', mixBlendMode: 'multiply', display: 'block' }} />
+          <img src="/logo.png" alt="ThemisIQ" style={{ height: 48, width: "auto", mixBlendMode: "multiply", display: 'block' }} />
         </a>
 
         {/* DESKTOP NAV */}
